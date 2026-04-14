e2c94875df77eb313f613018507a1185ccd6c729
YacqubJAD <yadi0001@stud.kea.dk>
2026-04-14 14:18:21 +0200

    hvor designforslaget lægger

---
+++ src/main/java/org/example/investingai/InvestingAiApplication.java
@@ -0,0 +1,13 @@
+package org.example.investingai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestingAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestingAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/investingai/InvestingAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.investingai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestingAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
